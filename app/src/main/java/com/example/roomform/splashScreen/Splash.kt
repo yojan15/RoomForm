479c9f94aa92fbc964b5fedd935b5e124e02a3c6
yojan khuble <85030838+yojan15@users.noreply.github.com>
2024-02-07 22:17:07 +0530

    ui done for signup and signin

--- app/src/main/java/com/example/roomform/splashScreen/Splash.kt
+++ app/src/main/java/com/example/roomform/splashScreen/Splash.kt
@@ -1,12 +1,31 @@
 package com.example.roomform.splashScreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.roomform.R
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import com.example.roomform.MainActivity
+import com.example.roomform.databinding.ActivitySplashBinding
 
 class Splash : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000 )
+
     }
 }
