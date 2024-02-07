40cfeeb95145ad8963938f1572f3a22c859eecf5
yojan khuble <85030838+yojan15@users.noreply.github.com>
2024-02-07 20:03:19 +0530

    file structure created

---
+++ app/src/main/java/com/example/roomform/splashScreen/Splash.kt
@@ -0,0 +1,12 @@
+package com.example.roomform.splashScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.roomform.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
